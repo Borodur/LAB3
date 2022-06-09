d0a75f6c8039602948cff9e9b55d06c33739d8dc
Borodur <matt.sol@o2.pl>
2022-06-09 21:53:04 +0200

    Zapomniałem wrzucić tego wcześniej

---
+++ src/pl.lublin.wsei.java.cwiczenia/java/pl/lublin/wsei/java/cwiczenia/Application.java
@@ -0,0 +1,28 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("gusInfoGraphic.fxml"));
+        Parent root = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        controller.setHostServices(this.getHostServices());
+        controller.setStage(stage);
+
+        stage.setTitle("Infografiki");
+        stage.setScene(new Scene(root, 800, 700));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
